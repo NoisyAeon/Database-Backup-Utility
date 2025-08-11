e5f0e3654de1a13278d7b58f25bbdf5329ee8a37
seejs <johannes-sebastian.see@fau.de>
2025-08-11 14:54:35 +0200

    Created folder structure and empty classes

---
+++ src/Database-Types/Database.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_Backup_Utility.src.Database_Types
+{
+    abstract class Database
+    {
+    }
+}
