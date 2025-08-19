b5f654aaee32f06797949a5e9d90629314499fad
seejs <johannes-sebastian.see@fau.de>
2025-08-19 16:24:53 +0200

    Implemented PostgreSQLDatabase

--- src/DBMS/PostgreSQLDatabase.cs
+++ src/DBMS/PostgreSQLDatabase.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Database_Backup_Utility.FileLogger;
 using Database_Backup_Utility.src.Database_Types;
+using Npgsql;
 
 namespace Database_Backup_Utility.src.DBMS
 {
-    //class PostgreSQLDatabase: Database<>
-    //{
-    //    public override void Connect()
-    //    {
-    //        // ToDo
-    //    }
-    //}
+    public class PostgreSQLDatabase : Database<NpgsqlConnection>
+    {
+        public override NpgsqlConnection Connection { get; protected set; }
+        public PostgreSQLDatabase(string server, string name, string user, string password) : base(server, name, user,
+            password)
+        {
+            Connection = new NpgsqlConnection(ConnectionString);
+            Connection.Open();
+            Log.Debug($"Successfully opened connection to database {Name} from PostgreSQL Server at {Server}");
+
+        }
+
+        public override void Dispose()
+        {
+            Connection.Close();
+            Log.Debug($"Closed connection to database {Name}");
+
+        }
+
+    }
 }
